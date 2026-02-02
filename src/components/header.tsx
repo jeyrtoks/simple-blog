5ca9cf21ccf7835cb66c18117ed4be3b3e2217f6
Joshua <jrapneda@gmail.com>
2026-02-03 03:22:40 +0800

    added working signup auth with working supabase integration; tweak ui

---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import "../App.css";
+
+const header = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      <p>Simple Blog!</p>
+      <h2>Hello, Welcome to My Blog!</h2>
+      <Link to="/signup">
+        <button title="Create post" style={{}}>
+          Create
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default header;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,40 +1,92 @@
-import { useEffect, useState } from "react"
-import { Form, Link } from "react-router-dom"
+import React, { useState } from "react";
+import { Form, Link, useNavigate } from "react-router-dom";
+import { signUpNewUser } from "../services/Auth";
 
 const Signup = () => {
-    document.title = "Signup"
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
+  document.title = "Signup";
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    return (
-        <>  
-            <p>{`${username} ${email} ${password}`}</p>
-            <Form className="card">
-                <h2 style={{textAlign: "center"}}>Create an Account</h2>
-                <input 
-                    onChange={(e) => {setUsername(e.target.value)}} 
-                    name="username" 
-                    type="text" 
-                    placeholder="Username..."/>
-                <input 
-                    onChange={(e) => {setEmail(e.target.value)}} 
-                    name="email" 
-                    type="text" 
-                    placeholder= "Email..."/>
-                <input 
-                    onChange={(e) => {setPassword(e.target.value)}} 
-                    name="password" 
-                    type="password" 
-                    placeholder="Password..."/> 
+  const handleSignUp = async (e: React.FormEvent) => {
+    e.preventDefault;
+    setLoading(true);
 
-                <button type="submit" disabled={loading}>Sign Up</button>
-            </Form>
-            <p>Already have an account? <Link to="/signin">Sign in!</Link></p>
-        </>
-    )
-}
+    if (!username) {
+      setError("Please add a username!");
+    } else if (!email) {
+      setError("Please add an email!");
+    } else if (!password) {
+      setError("Please add a password!");
+    }
 
-export default Signup
+    try {
+      const result = await signUpNewUser(email, password);
+      if (result.success) {
+        navigate("/");
+      }
+    } catch (err) {
+      console.log("Error signing up:", err);
+      setError("Failed to sign up. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Form className="card">
+        <h2 style={{ textAlign: "center" }}>Create an Account</h2>
+        <input
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          name="username"
+          type="text"
+          placeholder="Username..."
+        />
+        <input
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          name="email"
+          type="email"
+          placeholder="Email..."
+        />
+        <input
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          name="password"
+          type="password"
+          placeholder="Password..."
+        />
+        <div>
+          {error && (
+            <p
+              style={{
+                fontSize: "0.875rem",
+                lineHeight: "1.25rem",
+                color: "#ef4444",
+              }}
+            >
+              {error}
+            </p>
+          )}
+        </div>
+
+        <button onClick={handleSignUp} disabled={loading}>
+          {loading ? "Signing Up..." : "Sign Up"}
+        </button>
+      </Form>
+      <p>
+        Already have an account? <Link to="/signin">Sign in!</Link>
+      </p>
+    </>
+  );
+};
+
+export default Signup;
